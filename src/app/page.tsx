207f0df2789d764853726f39d53b8f17533422bb
Rodagency10 <rodriguepuh@gmail.com>
2024-08-22 15:07:40 +0000

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import SignIn from "./auth/signin/page";
+import AuthLayout from "@/components/Layouts/AuthLayout";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+    "Dipharma | Plateforme de pharmacie digitale",
+  description: "Plateforme de pharmacie digitale",
 };
 
 export default function Home() {
   return (
     <>
-      <DefaultLayout>
-        <ECommerce />
-      </DefaultLayout>
+      <AuthLayout>
+        <SignIn />
+      </AuthLayout>
     </>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -95,7 +95,7 @@ const Header = (props: {
 
               <input
                 type="text"
-                placeholder="Type to search..."
+                placeholder="Rechercher..."
                 className="w-full bg-transparent pl-9 pr-4 font-medium focus:outline-none xl:w-125"
               />
             </div>
@@ -103,20 +103,7 @@ const Header = (props: {
         </div>
 
         <div className="flex items-center gap-3 2xsm:gap-7">
-          <ul className="flex items-center gap-2 2xsm:gap-4">
-            {/* <!-- Dark Mode Toggler --> */}
-            <DarkModeSwitcher />
-            {/* <!-- Dark Mode Toggler --> */}
-
-            {/* <!-- Notification Menu Area --> */}
-            <DropdownNotification />
-            {/* <!-- Notification Menu Area --> */}
-
-            {/* <!-- Chat Notification Area --> */}
-            <DropdownMessage />
-            {/* <!-- Chat Notification Area --> */}
-          </ul>
-
+    
           {/* <!-- User Area --> */}
           <DropdownUser />
           {/* <!-- User Area --> */}
